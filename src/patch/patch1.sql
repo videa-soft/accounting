7050c9c0cfd2cd693cfd6322b520ac830c064e81
ghazaleh-sh <gh.nshahrokhi@gmail.com>
2016-03-19 13:28:12 +0330

    Update patch1.sql

--- src/patch/patch1.sql
+++ src/patch/patch1.sql
@@ -1,29 +1,39 @@
 CREATE TABLE USER (
-  id NOT NULL PRIMARY KEY,
+  user_id NOT NULL PRIMARY KEY,
   first_name VARCHAR(50),
   last_name VARCHAR(50),
-  customer_number VARCHAR(20),
+  customer_number VARCHAR(20)NOT NULL UNIQUE,
   home_address VARCHAR(500),
-  work_address VARCHAR(500),
   phone_number VARCHAR(15),
   national_code VARCHAR(10),
-  family_count INT
+  family_count INT NOT NULL 
 );
 
 
 CREATE TABLE ACCOUNT_BALANCE (
-  id NOT NULL PRIMARY KEY,
+  acc_id NOT NULL PRIMARY KEY,
   createDate DATE,
   debit INT,
   credit INT,
   account_balance INT,
   description VARCHAR(1000),
-  customer_number VARCHAR(20)
+  user_id INT FOREIGN KEY REFERENCES USER(user_id)
 );
 
 
 
 CREATE TABLE BILL (
-
-
+  bill_id NOT NULL PRIMARY KEY,
+  user_id INT FOREIGN KEY REFERENCES USER(user_id)
+  previous_Date DATE,
+  Current_Date DATE,
+  previous_figure INT,
+  current_figure INT,
+  cunsumption INT,
+  abonman FLOAT,
+  reduction INT,
+  services INT,
+  cost_water INT,
+  cost_balance INT,
+  final_amount INT
 );
